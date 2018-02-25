06bff5f3df9fa6a63560154676f215031b4d6b7b
faraonc <faraonc@outlook.com>
2018-02-25 12:39:37 -0800

    baseline master.

--- scripts/app.js
+++
@@ -1,8 +0,0 @@
-new Vue({
-   el: '#gen-table',
-   data: {
-       headers: ['Coculture', 0, 100, 300, 500, 780, 1000, 'Compare'],
-       cultures_x: ['HA3', 'HE3', 'HR2', 'HS3', 'UA3', 'UE3', 'UR1','US1','Compare'],
-       rows: ['gen-0', 'gen-100', 'gen-300', 'gen-500', 'gen-780', 'gen-1000', 'gen-compare']
-   }
-});

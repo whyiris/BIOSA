728791119a8c7db515fd90eba6cf0412f836eb96
Wai Kwan Shum <wai.swk@D-10-156-10-165.dhcp4.washington.edu>
2018-03-11 15:01:22 -0700

    updated db.js

--- scripts/db.js
+++ scripts/db.js
@@ -1,6 +1,6 @@
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/"; //Change the port for deployment.
-var dbName = "ourDB";
+var dbName = "BIOSA";
 
 
 //TODO add document about parameters
